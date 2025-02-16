0695634c7b202bf1f29a12cb7d68bd5d04c0b2d1
Manuel Amstutz <manuel.amstutz@orphis.ch>
2025-02-16 09:32:41 +0100

    Added TemplateProcessorExtension interface

---
+++ DocxTemplater/Extensions/ITemplateProcessorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using DocumentFormat.OpenXml;
+using DocxTemplater.Formatter;
+
+namespace DocxTemplater.Extensions
+{
+    public interface ITemplateProcessorExtension
+    {
+        void PreProcess(OpenXmlCompositeElement content);
+
+        void BlockContentExtracted(IReadOnlyCollection<OpenXmlElement> content);
+
+        void ReplaceVariables(IVariableReplacer variableReplacer, IModelLookup models, OpenXmlElement parentNode, List<OpenXmlElement> newContent);
+    }
+}
