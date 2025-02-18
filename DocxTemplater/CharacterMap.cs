b761dd1b7e75a1ba81a985f9ec0a7fd89ef111ee
Manuel Amstutz <manuel.amstutz@orphis.ch>
2025-02-18 20:35:57 +0100

    Avoid recreation of CharacterMap to improve performance

--- DocxTemplater/CharacterMap.cs
+++ DocxTemplater/CharacterMap.cs
@@ -1,20 +1,22 @@
-﻿using System.Text;
-using DocumentFormat.OpenXml;
+﻿using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 
 
 namespace DocxTemplater
 {
-    internal record struct Character(char Char, Text Element, int CharIndexInText);
+    internal record struct CharacterPointer(char Char, Text Element, int CharIndexInText, int Index);
 
     internal class CharacterMap
     {
-        private readonly Character[] m_map;
+        private readonly CharacterPointer[] m_map;
         private readonly OpenXmlCompositeElement m_rootElement;
         private readonly StringBuilder m_stringBuilder;
 
-        public Character this[int index] => m_map[index];
+        public CharacterPointer this[int index] => m_map[index];
 
         public string Text { get; private set; }
 
@@ -22,7 +24,7 @@ namespace DocxTemplater
         {
             m_rootElement = ce;
             Text = ce.InnerText;
-            m_map = new Character[Text.Length];
+            m_map = new CharacterPointer[Text.Length];
             m_stringBuilder = new StringBuilder(Text.Length);
             Recreate();
         }
@@ -36,11 +38,113 @@ namespace DocxTemplater
                 m_stringBuilder.Append(text.Text);
                 for (var charIndexInText = 0; charIndexInText < text.Text.Length; ++charIndexInText)
                 {
-                    m_map[index++] = new Character(text.Text[charIndexInText], text, charIndexInText);
+                    m_map[index] = new CharacterPointer(text.Text[charIndexInText], text, charIndexInText, index);
+                    ++index;
                 }
             }
 
             Text = m_stringBuilder.ToString();
+        }
+
+
+        public CharacterPointer MergeText(CharacterPointer first, CharacterPointer last)
+        {
+            var commonParent = first.Element.FindCommonParent(last.Element) ?? throw new ArgumentException("Text elements must have a common parent");
+            if (first.CharIndexInText < 0 || first.CharIndexInText >= first.Element.Text.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(first));
+            }
+            var matchLength = last.Index - first.Index + 1;
+            var matchInSameElement = first.Element == last.Element;
+            if (first.CharIndexInText != 0) // leading text is not part of the match
+            {
+                // new last part
+                Text newPart = first.Element.SplitAtIndexAndReturnLastPart(first.CharIndexInText);
+                var index = first.Index;
+                InsertNewTextAddIndex(index, newPart);
+                first = m_map[index];
+            }
+            if (first.CharIndexInText + matchLength < first.Element.Text.Length) // trailing text part of the match in the first element
+            {
+                var newPart = first.Element.SplitAtIndexAndReturnLastPart(first.CharIndexInText + matchLength);
+                InsertNewTextAddIndex(first.Index + matchLength, newPart);
+            }
+
+            if (matchInSameElement)
+            {
+                return first;
+            }
+
+            List<OpenXmlElement> toRemove = new();
+            bool found = false;
+            foreach (var current in commonParent.Descendants<Text>())
+            {
+                if (current == first.Element)
+                {
+                    found = true;
+                    continue;
+                }
+                if (!found)
+                {
+                    continue;
+                }
+
+                // trailing text is not part of the match int the last element
+                if (first.Element.Text.Length + current.Text.Length > matchLength)
+                {
+                    var firstPart = current.Text[..(matchLength - first.Element.Text.Length)];
+                    var secondPart = current.Text[(matchLength - first.Element.Text.Length)..];
+                    AddTextToCharacterPointer(first, firstPart);
+                    current.Text = secondPart;
+                    FixMapAtIndex(first.Index + first.Element.Text.Length);
+                    break;
+                }
+                AddTextToCharacterPointer(first, current.Text);
+                toRemove.Add(current);
+                if (current == last.Element)
+                {
+                    break;
+                }
+            }
+            foreach (var text in toRemove)
+            {
+                text.RemoveWithEmptyParent();
+            }
+            return first;
+        }
+
+        private void FixMapAtIndex(int firstIndex)
+        {
+            if (firstIndex < m_map.Length)
+            {
+                var start = m_map[firstIndex];
+                if (start.Element != m_map[firstIndex - 1].Element)
+                {
+                    InsertNewTextAddIndex(firstIndex, start.Element);
+                }
+            }
+
+        }
+
+        private void InsertNewTextAddIndex(int startIndex, Text newPart)
+        {
+            for (int i = 0; i < newPart.Text.Length; i++)
+            {
+                var index = startIndex + i;
+                m_map[index] = new CharacterPointer(newPart.Text[i], newPart, i, index);
+            }
+        }
+
+        private void AddTextToCharacterPointer(CharacterPointer characterPointer, string appendText)
+        {
+            var textStart = characterPointer.Index - characterPointer.CharIndexInText;
+            for (int i = 0; i < appendText.Length; i++)
+            {
+                var textOffest = characterPointer.Element.Text.Length + i;
+                var index = textStart + textOffest;
+                m_map[index] = new CharacterPointer(appendText[i], characterPointer.Element, textOffest, index);
+            }
+            characterPointer.Element.Text += appendText;
         }
     }
 }
