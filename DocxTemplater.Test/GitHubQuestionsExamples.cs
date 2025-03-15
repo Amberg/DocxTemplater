c6926cc03a35034b2cebc3f9cd3af549dcdfbce9
Manuel Amstutz <manuel.amstutz@orphis.ch>
2025-03-15 07:39:40 +0100

    Fix Typo

--- DocxTemplater.Test/GitHubQuestionsExamples.cs
+++ DocxTemplater.Test/GitHubQuestionsExamples.cs
@@ -18,7 +18,7 @@ namespace DocxTemplater.Test
             memStream.Position = 0;
 
             var docTemplate = new DocxTemplate(memStream);
-            docTemplate.BindModel("Items", new[] { "Fist Line\r\n", "Second Line\r\n", "Third Line\r\n" });
+            docTemplate.BindModel("Items", new[] { "First Line\r\n", "Second Line\r\n", "Third Line\r\n" });
             var result = docTemplate.Process();
             docTemplate.Validate();
             Assert.That(result, Is.Not.Null);
@@ -27,7 +27,7 @@ namespace DocxTemplater.Test
             var document = WordprocessingDocument.Open(result, false);
             var body = document.MainDocumentPart.Document.Body;
             Assert.That(body.InnerXml,
-                Is.EqualTo(@"<w:p xmlns:w=""http://schemas.openxmlformats.org/wordprocessingml/2006/main""><w:r><w:t>Fist Line</w:t><w:br /><w:t>Second Line</w:t><w:br /><w:t>Third Line</w:t><w:br /></w:r></w:p>"));
+                Is.EqualTo(@"<w:p xmlns:w=""http://schemas.openxmlformats.org/wordprocessingml/2006/main""><w:r><w:t>First Line</w:t><w:br /><w:t>Second Line</w:t><w:br /><w:t>Third Line</w:t><w:br /></w:r></w:p>"));
         }
     }
 }
