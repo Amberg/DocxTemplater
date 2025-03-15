0e27b994f95939d5eadd6f221541f1b507a66958
Manuel Amstutz <manuel.amstutz@orphis.ch>
2025-03-15 06:59:17 +0100

    Fix: New line a the end of a text created two line breaks

---
+++ DocxTemplater.Test/GitHubQuestionsExamples.cs
@@ -0,0 +1,33 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocxTemplater.Test
+{
+    class GitHubQuestionsExamples
+    {
+
+        [Test]
+        public void EachItemOnNewLine()
+        {
+            using var memStream = new MemoryStream();
+            using var wpDocument = WordprocessingDocument.Create(memStream, WordprocessingDocumentType.Document);
+            MainDocumentPart mainPart = wpDocument.AddMainDocumentPart();
+            mainPart.Document = new Document(new Body(new Paragraph(new Run(new Text("{{#Items}}{{.}}{{/Items}}")))));
+            wpDocument.Save();
+            memStream.Position = 0;
+
+            var docTemplate = new DocxTemplate(memStream);
+            docTemplate.BindModel("Items", new[] { "Fist Line\r\n", "Second Line\r\n", "Third Line\r\n" });
+            var result = docTemplate.Process();
+            docTemplate.Validate();
+            Assert.That(result, Is.Not.Null);
+            result.SaveAsFileAndOpenInWord();
+            // check document contains 2 altChunks
+            var document = WordprocessingDocument.Open(result, false);
+            var body = document.MainDocumentPart.Document.Body;
+            Assert.That(body.InnerXml,
+                Is.EqualTo(@"<w:p xmlns:w=""http://schemas.openxmlformats.org/wordprocessingml/2006/main""><w:r><w:t>Fist Line</w:t><w:br /><w:t>Second Line</w:t><w:br /><w:t>Third Line</w:t><w:br /></w:r></w:p>"));
+        }
+    }
+}
