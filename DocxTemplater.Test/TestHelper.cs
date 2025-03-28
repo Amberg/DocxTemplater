ede74d9d9012e45b2ff4f1c2052a00e2637140e0
Manuel Amstutz <manuel.amstutz@orphis.ch>
2025-03-28 11:10:37 +0100

    Prevent TestHelper to open too many Documents in Debug Mode

--- DocxTemplater.Test/TestHelper.cs
+++ DocxTemplater.Test/TestHelper.cs
@@ -5,16 +5,20 @@ namespace DocxTemplater.Test
 {
     internal static class TestHelper
     {
+        private static int s_openCounter = 0;
+
         public static void SaveAsFileAndOpenInWord(this Stream stream, string extension = "docx")
         {
 #if RELEASE
             return;
 #pragma warning disable CS0162
 #endif
-            if (Environment.GetEnvironmentVariable("DOCX_TEMPLATER_VISUAL_TESTING") == null)
+            if (Environment.GetEnvironmentVariable("DOCX_TEMPLATER_VISUAL_TESTING") == null || s_openCounter > 1)
             {
                 return;
             }
+
+            s_openCounter++;
             stream.Position = 0;
             var fileName = Path.ChangeExtension(Path.GetTempFileName(), extension);
             using (var fileStream = File.OpenWrite(fileName))
